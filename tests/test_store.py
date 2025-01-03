ef1cdfcdff1c774e6ffb1eaf129186b1c8cecb2a
Almaz <Almazt96@gmail.com>
2025-01-03 07:25:06 +0300

    unit test example test added

--- tests/test_store.py
+++ tests/test_store.py
@@ -1,2 +1,11 @@
 def test_store():
     assert 1 + 1 == 2
+
+import unittest
+
+class TestExample(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(2 + 2, 4)
+
+if __name__ == '__main__':
+    unittest.main()
